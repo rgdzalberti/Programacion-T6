2d78de54e0e531a3b5827a950beaa685e633b8ef
Ricardo <rgallego@iesrafaelalberti.es>
2022-01-19 20:41:10 +0100

    .

--- src/main/kotlin/DAM1_6_3_RGD.kt
+++ src/main/kotlin/DAM1_6_3_RGD.kt
@@ -11,7 +11,9 @@ no puede contener código. El secundario hace uso del constructor interior de la
 cuando el constructor primario no satisface las necesidades.
 
 3 ¿De qué manera afecta y como hay que usar los constructores en la herencia?
-
+Cuando se ejecuta un constructor heredado, también lo hará el constructor de la superclase.
+Lo que se debe hacer es ejecutar en primer lugar el constructor de la clase que ocupa el nivel más
+alto en la jerarquía de herencia y se continúa de forma ordenada con el resto de las subclases.
 
 
  */
